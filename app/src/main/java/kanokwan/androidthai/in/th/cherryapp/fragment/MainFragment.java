05c97d3b9b369631b765212c686325c59cd36a37
masterUNG <phrombutr@gmail.com>
2018-02-17 19:10:04 -0800

    Repolace Fragment Scuuess

--- app/src/main/java/kanokwan/androidthai/in/th/cherryapp/fragment/MainFragment.java
+++ app/src/main/java/kanokwan/androidthai/in/th/cherryapp/fragment/MainFragment.java
@@ -4,8 +4,10 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.TextureView;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import kanokwan.androidthai.in.th.cherryapp.R;
 
@@ -13,7 +15,30 @@ import kanokwan.androidthai.in.th.cherryapp.R;
  * Created by MasterUNG on 2/16/2018.
  */
 
-public class MainFragment extends Fragment{
+public class
+MainFragment extends Fragment{
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+//        register controller
+        TextView textView= getView().findViewById(R.id.textViewRegister);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                Replace Fragment
+                getActivity().getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.contentMainFragment, new RegisterFragment())
+                        .addToBackStack(null)
+                        .commit();
+            }
+        });
+
+
+
+    } // Main Method
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater,
---
+++ app/src/main/java/kanokwan/androidthai/in/th/cherryapp/fragment/RegisterFragment.java
@@ -0,0 +1,23 @@
+package kanokwan.androidthai.in.th.cherryapp.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import kanokwan.androidthai.in.th.cherryapp.R;
+
+/**
+ * Created by MasterUNG on 2/17/2018.
+ */
+
+public class RegisterFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_register, container, false);
+        return view;
+    }
+} //Main class
